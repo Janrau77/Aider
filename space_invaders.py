e0bc2b5605e07e2ea83a92e740ca9a17cd8af0c2
Janrau77 <Janne.rautell@gmail.com>
2025-11-25 04:31:14 +0200

    fix: Correct player movement and prevent out-of-bounds movement

--- space_invaders.py
+++ space_invaders.py
@@ -50,9 +50,9 @@ class Player(pygame.sprite.Sprite):
         self.speed_x = 0
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            self.speed_x = -PLAYER_SPEED
+            self.speed_x -= PLAYER_SPEED
         if keys[pygame.K_RIGHT]:
-            self.speed_x = PLAYER_SPEED
+            self.speed_x += PLAYER_SPEED
         self.rect.x += self.speed_x
         if self.rect.left < 0:
             self.rect.left = 0
@@ -165,4 +165,3 @@ while running:
     clock.tick(60) # 60 FPS
 
 pygame.quit()
-```
